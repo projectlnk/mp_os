f3a943bcacbe782f759e80be6851f233983c8ed2
projectlnk <164139429+projectlnk@users.noreply.github.com>
2024-10-20 20:29:52 +0300

    Update client_logger_tests.cpp
    added a test for logger

--- logger/client_logger/tests/client_logger_tests.cpp
+++ logger/client_logger/tests/client_logger_tests.cpp
@@ -1,4 +1,7 @@
 #include <gtest/gtest.h>
+#include "client_logger.h"
+#include "logger.h"
+#include "client_logger_builder.h"
 
 
 
@@ -6,7 +9,18 @@ int main(
     int argc,
     char *argv[])
 {
-    testing::InitGoogleTest(&argc, argv);
+    auto* build_logger_1 = new client_logger_builder;
 
-    return RUN_ALL_TESTS();
+    build_logger_1->add_file_stream("D:\what.txt", logger::severity::information);
+
+    build_logger_1->set_format("%t %d %m %s");
+
+    logger* logger_1 = build_logger_1->build();
+
+    logger_1->log("Zeleniy Slonik", logger::severity::information);
+
+    delete build_logger_1;
+    delete logger_1;
+
+    return 0;
 }

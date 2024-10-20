8c940d9be7a91022ff78ac246b827e15adcdc990
projectlnk <164139429+projectlnk@users.noreply.github.com>
2024-10-20 20:27:16 +0300

    Update client_logger_builder.h
    made a logger

--- logger/client_logger/include/client_logger_builder.h
+++ logger/client_logger/include/client_logger_builder.h
@@ -2,45 +2,53 @@
 #define MATH_PRACTICE_AND_OPERATING_SYSTEMS_CLIENT_LOGGER_BUILDER_H
 
 #include <logger_builder.h>
+#include <client_logger.h>
+#include <map>
 
-class client_logger_builder final:
+class client_logger_builder final :
     public logger_builder
+
+
 {
+private:
+    std::map<std::string, unsigned char> file_info;
+    std::string format;
 
 public:
+    logger_builder* set_format(const std::string& format_str);
 
     client_logger_builder();
 
     client_logger_builder(
-        client_logger_builder const &other);
+        client_logger_builder const& other);
 
-    client_logger_builder &operator=(
-        client_logger_builder const &other);
+    client_logger_builder& operator=(
+        client_logger_builder const& other);
 
     client_logger_builder(
-        client_logger_builder &&other) noexcept;
+        client_logger_builder&& other) noexcept;
 
-    client_logger_builder &operator=(
-        client_logger_builder &&other) noexcept;
+    client_logger_builder& operator=(
+        client_logger_builder&& other) noexcept;
 
     ~client_logger_builder() noexcept override;
 
 public:
 
-    logger_builder *add_file_stream(
-        std::string const &stream_file_path,
+    logger_builder* add_file_stream(
+        std::string const& stream_file_path,
         logger::severity severity) override;
 
-    logger_builder *add_console_stream(
+    logger_builder* add_console_stream(
         logger::severity severity) override;
 
     logger_builder* transform_with_configuration(
-        std::string const &configuration_file_path,
-        std::string const &configuration_path) override;
+        std::string const& configuration_file_path,
+        std::string const& configuration_path) override;
 
-    logger_builder *clear() override;
+    logger_builder* clear() override;
 
-    [[nodiscard]] logger *build() const override;
+    [[nodiscard]] logger* build() const override;
 
 };
 

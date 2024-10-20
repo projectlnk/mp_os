226fb7e9a7e4f2783ac59cdc31036f87d91acc22
projectlnk <projectlaniakea1108@gmail.com>
2024-10-20 11:31:06 +0300

    start adding ro5 to client_logger

--- logger/client_logger/include/client_logger.h
+++ logger/client_logger/include/client_logger.h
@@ -2,32 +2,43 @@
 #define MATH_PRACTICE_AND_OPERATING_SYSTEMS_CLIENT_LOGGER_H
 
 #include <logger.h>
+#include <map>
 #include "client_logger_builder.h"
 
-class client_logger final:
+class client_logger final :
     public logger
 {
+
+    friend class client_logger_builder;
+private:
+
+    static std::map <std::string, std::pair<std::ofstream*, int>> logger_info;
+    std::map<std::string, unsigned char> file_info;
+    std::string format;
+
+private:
+    client_logger(std::map<std::string, unsigned char> file_info, std::string file_name);
 
 public:
 
     client_logger(
-        client_logger const &other);
+        client_logger const& other);
 
-    client_logger &operator=(
-        client_logger const &other);
+    client_logger& operator=(
+        client_logger const& other);
 
     client_logger(
-        client_logger &&other) noexcept;
+        client_logger&& other) noexcept;
 
-    client_logger &operator=(
-        client_logger &&other) noexcept;
+    client_logger& operator=(
+        client_logger&& other) noexcept;
 
     ~client_logger() noexcept final;
 
 public:
 
-    [[nodiscard]] logger const *log(
-        const std::string &message,
+    [[nodiscard]] logger const* log(
+        const std::string& message,
         logger::severity severity) const noexcept override;
 
 };
